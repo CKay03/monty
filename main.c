852ff76b34480fcafc8be54e2c1e8af63163ce84
CKay03 <fiati.charles@gmail.com>
2024-01-21 18:54:06 -0800

    stacks and queues

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
-#include "monty.h"
+#define _GNU_SOURCE
 
+#include "monty.h"
+#include <stdio.h>
+
+bus_t bus = {NULL, NULL, NULL, 0};
 /**
 * main - monty code interpreter
 * @argc: number of arguments
